b9133a1789a426025f5351c7f4fda1f7e1a03957
abhishek tiwari <abhishektiwari@abhisheks-MacBook-Air.local>
2026-04-05 19:26:05 +0530

    fix pdf download feature

--- ai-saas-backend/src/services/ai.service.js
+++ ai-saas-backend/src/services/ai.service.js
@@ -58,21 +58,23 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 
 async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
-
-    const pdfBuffer = await page.pdf({
-        format: "A4", margin: {
-            top: "20mm",
-            bottom: "20mm",
-            left: "15mm",
-            right: "15mm"
-        }
+    const browser = await puppeteer.launch({
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',  // ✅ important for Render
+            '--disable-gpu'
+        ],
+        headless: true
     })
-
+    const page = await browser.newPage()
+    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
+    const pdfBuffer = await page.pdf({
+        format: "A4",
+        margin: { top: "20mm", bottom: "20mm", left: "15mm", right: "15mm" }
+    })
     await browser.close()
-
     return pdfBuffer
 }
 
